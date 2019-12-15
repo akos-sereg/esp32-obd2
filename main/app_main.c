8e5f4a7ea548dd9213f81b692157680306fbc505
akos.sereg <akos.sereg@gmail.com>
2019-12-15 22:27:34 +0100

    Bluetooth reconnect

--- main/app_main.c
+++ main/app_main.c
@@ -23,6 +23,7 @@ void main_task(void * pvParameter)
     int obd2_connected_displaying = 0;
     int obd2_connected_displayed = 0;
     int obd2_connected_displaying_elapsed_ms = 0;
+    int obd2_connecting_elapsed_ms = 0;
 
     engine_load_init();
     setup_switches();
@@ -34,26 +35,42 @@ void main_task(void * pvParameter)
     while(1) {
 	engine_load_set(cnt);
 	cnt++;
+
 	if (cnt == 10) {
 	    cnt = 0;
 	    vTaskDelay(500 / portTICK_RATE_MS);
 	    if (obd2_connected_displaying) {
 		obd2_connected_displaying_elapsed_ms += 500;
 	    }
+	    if (!bt_is_connected) {
+		obd2_connecting_elapsed_ms += 500;
+	    }
 	}
 
+	// connected to bluetooth, notify user on display for a couple of seconds
 	if (bt_is_connected && !obd2_connected_displaying && !obd2_connected_displayed) {
 	    lcd_display_text("Connected to", "OBD2");
 	    obd2_connected_displaying = 1;
 	    obd2_connected_displayed = 1;
 	}
 
+	// connected to bluetooth OBD2, display data
 	if (obd2_connected_displaying) {
 	    obd2_connected_displaying_elapsed_ms += 50;
 	    if (obd2_connected_displaying_elapsed_ms > 3000) {
 		obd2_connected_displaying = 0;
 		refresh_lcd_display();
 	    }
+	}
+
+	if (!bt_is_connected) {
+	    obd2_connecting_elapsed_ms += tick_rate_ms;
+	}
+
+	if (obd2_connecting_elapsed_ms > 10000) {
+	    lcd_display_text("Connecting to", "OBD2 (2)");
+	    init_bluetooth();
+	    //lcd_display_text("Connected to", "OBD2 (2)");
 	}
 
 	vTaskDelay(tick_rate_ms / portTICK_RATE_MS);
