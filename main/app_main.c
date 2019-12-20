8b36e887a77ab587b707b956114af1b64af209ba
Akos Sereg <akos.sereg@logmein.com>
2019-12-20 21:10:51 +0100

    State handling

--- main/app_main.c
+++ main/app_main.c
@@ -13,68 +13,72 @@
 #include "include/lcd.h"
 #include "include/bluetooth.h"
 #include "include/protocol.h"
+#include "include/state.h"
 
 #define TAG "app"
 
 void main_task(void * pvParameter)
 {
-    int cnt = 0;
-    int tick_rate_ms = 50;
-    int obd2_connected_displaying = 0;
-    int obd2_connected_displayed = 0;
-    int obd2_connected_displaying_elapsed_ms = 0;
-    int obd2_connecting_elapsed_ms = 0;
+  int cnt = 0;
+  int tick_rate_ms = 50;
 
-    engine_load_init();
-    setup_switches();
-    i2c_master_init();
-    // refresh_lcd_display();
+  app_state.obd2_bluetooth.displaying_connected = 0;
+  app_state.obd2_bluetooth.displaying_connected_elapsed_ms = 0;
+  app_state.obd2_bluetooth.displaying_connecting_elapsed_ms = 0;
+  app_state.obd2_bluetooth.displayed_connected = 0;
 
-    lcd_display_text("Connecting to", "Bluetooth OBD2");
+  engine_load_init();
+  setup_switches();
+  i2c_master_init();
 
-    while(1) {
-	engine_load_set(cnt);
-	cnt++;
+  lcd_display_text("Connecting to", "Bluetooth OBD2");
 
-	if (cnt == 10) {
-	    cnt = 0;
-	    vTaskDelay(500 / portTICK_RATE_MS);
-	    if (obd2_connected_displaying) {
-		obd2_connected_displaying_elapsed_ms += 500;
-	    }
-	    if (!bt_is_connected) {
-		obd2_connecting_elapsed_ms += 500;
-	    }
-	}
+  while(1) {
+    engine_load_set(cnt);
+    cnt++;
 
-	// connected to bluetooth, notify user on display for a couple of seconds
-	if (bt_is_connected && !obd2_connected_displaying && !obd2_connected_displayed) {
-	    lcd_display_text("Connected to", "OBD2");
-	    obd2_connected_displaying = 1;
-	    obd2_connected_displayed = 1;
-	}
-
-	// connected to bluetooth OBD2, display data
-	if (obd2_connected_displaying) {
-	    obd2_connected_displaying_elapsed_ms += 50;
-	    if (obd2_connected_displaying_elapsed_ms > 3000) {
-		obd2_connected_displaying = 0;
-		refresh_lcd_display();
-	    }
-	}
-
-	if (!bt_is_connected) {
-	    obd2_connecting_elapsed_ms += tick_rate_ms;
-	}
-
-	if (obd2_connecting_elapsed_ms > 10000) {
-	    lcd_display_text("Connecting to", "OBD2 (2)");
-	    init_bluetooth();
-	    //lcd_display_text("Connected to", "OBD2 (2)");
-	}
-
-	vTaskDelay(tick_rate_ms / portTICK_RATE_MS);
+    if (cnt == 10) {
+      cnt = 0;
+      vTaskDelay(500 / portTICK_RATE_MS);
+      if (app_state.obd2_bluetooth.displaying_connected) {
+        app_state.obd2_bluetooth.displaying_connected_elapsed_ms += 500;
+      }
+      if (!bt_is_connected) {
+        app_state.obd2_bluetooth.displaying_connecting_elapsed_ms += 500;
+      }
     }
+
+    // connected to bluetooth, notify user on display for a couple of seconds
+    if (bt_is_connected
+      && !app_state.obd2_bluetooth.displaying_connected
+      && !app_state.obd2_bluetooth.displayed_connected) {
+        lcd_display_text("Connected to", "OBD2");
+        app_state.obd2_bluetooth.displaying_connected = 1;
+        app_state.obd2_bluetooth.displayed_connected = 1;
+    }
+
+    // connected to bluetooth OBD2, display data
+    if (app_state.obd2_bluetooth.displaying_connected) {
+      app_state.obd2_bluetooth.displaying_connected_elapsed_ms += 50;
+      if (app_state.obd2_bluetooth.displaying_connected_elapsed_ms > 3000) {
+        app_state.obd2_bluetooth.displaying_connected = 0;
+        refresh_lcd_display();
+      }
+    }
+
+    if (!bt_is_connected) {
+        app_state.obd2_bluetooth.displaying_connecting_elapsed_ms += tick_rate_ms;
+    } else {
+        app_state.obd2_bluetooth.displaying_connecting_elapsed_ms = 0;
+    }
+
+    if (app_state.obd2_bluetooth.displaying_connecting_elapsed_ms > 10000) {
+        lcd_display_text("Connecting to", "OBD2 (2)");
+        esp_restart();
+    }
+
+    vTaskDelay(tick_rate_ms / portTICK_RATE_MS);
+  }
 }
 
 void app_main()
--- main/include/state.h
+++ main/include/state.h
@@ -1,8 +1,25 @@
-#ifndef __state_h_included__
-#define __state_h_included__
+#ifndef __app_state_h_included__
+#define __app_state_h_included__
 
-struct app_state {
-  int obd2_connected_displaying;
-};
+typedef struct app_state_t {
+  struct obd2_bluetooth {
+    /* if true, we are displaying the message "Connected to OBD2" on LCD display */
+    int displaying_connected;
+
+    /* number of milliseconds since we are displaying "Connected to OBD2" message */
+    int displaying_connected_elapsed_ms;
+
+    /* number of milliseconds since we are displaying "Connecting to OBD" message */
+    int displaying_connecting_elapsed_ms;
+
+    /*
+       if true, we already displayed "Connected to OBD2" message on LCD display for enough time,
+       and we should no longer have it displayed.
+    */
+    int displayed_connected;
+  } obd2_bluetooth;
+} app_state_t;
+
+extern app_state_t app_state;
 
 #endif
